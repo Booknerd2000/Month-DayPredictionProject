3ffe63d6e10c92dc58d1d4ec6a53a30a8dd882de
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-04-04 08:54:34 -0400

    Added a raw from of the day of week equation

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -2,9 +2,30 @@ package com.LickingHeights;
 import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args) {
+    public static int main(String[] args) {
 	Scanner keyboard;
 	keyboard = new Scanner(System.in);
-	
-    }
+	int year, month, day, j, q, m, k;
+
+		while (true) {
+			System.out.println("What year were you born?");
+			year= keyboard.nextInt();
+			k =year;
+			j =(k/100);
+			k=(k%100);
+
+			System.out.println("What month were you born?");
+			month= keyboard.nextInt();
+			m =month;
+			System.out.println("What day were you born?");
+			day= keyboard.nextInt();
+			q =day;
+			System.out.println((q+(13*(m+1)/5)+k+(k/4)+(j/4)-2*j)%7);
+		}
+		}
+	public static int birthMonth(int number) {
+		if (number == 1) {
+			return 13;
+		}
+	}
 }
