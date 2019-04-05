3caa0170db843a289aa3913d8654a9674df3e4b2
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-04-05 08:13:56 -0400

    added a birthmonth method and a birthyear method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -2,30 +2,47 @@ package com.LickingHeights;
 import java.util.Scanner;
 public class Main {
 
-    public static int main(String[] args) {
-	Scanner keyboard;
-	keyboard = new Scanner(System.in);
-	int year, month, day, j, q, m, k;
+	public static void main(String[] args) {
+		Scanner keyboard;
+        keyboard = new Scanner(System.in);
+		int year, month, day, j, q, m, k;
 
 		while (true) {
 			System.out.println("What year were you born?");
-			year= keyboard.nextInt();
-			k =year;
-			j =(k/100);
-			k=(k%100);
+			year = keyboard.nextInt();
+			k = year;
 
 			System.out.println("What month were you born?");
-			month= keyboard.nextInt();
-			m =month;
+			month = keyboard.nextInt();
+			m = month;
 			System.out.println("What day were you born?");
-			day= keyboard.nextInt();
-			q =day;
-			System.out.println((q+(13*(m+1)/5)+k+(k/4)+(j/4)-2*j)%7);
+			day = keyboard.nextInt();
+			q = day;
+            j = (k / 100);
+            k = (k % 100);
+            birthYear(m,k);
+            birthMonth(m);
+			System.out.println((q + (13 * (m + 1) / 5) + k + (k / 4) + (j / 4) - 2 * j) % 7);
 		}
-		}
-	public static int birthMonth(int number) {
-		if (number == 1) {
-			return 13;
-		}
+
+
 	}
+
+	public static int birthMonth(int dateM) {
+		if (dateM <= 1) {
+			return dateM + 12;
+		}
+		if (dateM == 2) {
+			return dateM + 12;
+		}
+		return dateM;
+	}
+	public static int birthYear(int dateM, int dateY){
+	    if (dateM <=2 ){
+	        return dateY -1;
+        }
+        return dateY;
+    }
 }
+
+
